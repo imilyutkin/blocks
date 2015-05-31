bcbabdec632ecfc456e8505740ea979bd1257e70
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2015-05-31 13:42:09 +0300

    upd alg

--- alg.js
+++ alg.js
@@ -1,46 +1,75 @@
- $(function() {
-        var numOfBlocks = Math.ceil(Math.sqrt($(":not(:has(>div))").length));
+        $(function() {
+            // var numOfBlocks = Math.ceil(Math.sqrt($(":not(:has(>div))").length)) + 1;
+            var numOfBlocks = Math.ceil(Math.sqrt($(":not(:has(*))").length)) + 1;
 
-        var root = $(':eq(0)');
-        var nodes = getSeparatedNodes(root, numOfBlocks);
-
-        $.each(function( index, value ) {
-            $(value.addClass('lol'));
+            // var root = $('div.container');
+            var root = $('body');
+            var nodes = getSeparatedNodes(root, numOfBlocks);
+            console.log(nodes.length)
+            markSelectedBlocks(nodes);
         });
-    });
 
-    function getSeparatedNodes(rootObj, numBlocks) {
-        var nodes = [rootObj];
+        function getSeparatedNodes(root, numOfBlocks) {
+            var nodes = [];
+            nodes.push(root);
 
-        while (nodes.length < numBlocks) {
-            var maxWeightNode = nodes[0];
-            var nodeIndex = 0;
+            while (nodes.length < numOfBlocks) {
+                var maxWeightNode = getMaxWeightBlock(nodes);
+                var nodeIndex = 0;
 
-            for (var i = 0; i < nodes.length; i++) {
-                if (((weight(nodes[i]) > weight(maxWeightNode))
-                              || ($(maxWeightNode).children().length == 1)) && ($(nodes[i]).children().length > 1)) {
-                    maxWeightNode = nodes[i];
-                    nodeIndex = i;
+                for (var i = 0; i < nodes.length; i++) {
+                    var currentWeight = weight(nodes[i]);                    
+                    console.log(weight(nodes[i]));
+                    if ((weight(nodes[i]) >= weight(maxWeightNode) || $(maxWeightNode).children().length == 1) 
+                        && $(nodes[i]).children().length > 0) {
+                        maxWeightNode = nodes[i];
+                        nodeIndex = i;
+                    }
+                    if (currentWeight < 1) {
+                        nodes.splice(i, 1)
+                    };
                 }
-                console.log(weight(nodes[i]));
-            }
 
-            if ($(maxWeightNode).children().length <= 1) {
-                break;
+                if ($(maxWeightNode).children().length < 1) {
+                    break;
+                }
+                nodes.splice(nodeIndex, 1);
+                
+                var children = $(maxWeightNode).children(':not(script)');
+                $.each(children, function(index, value) {
+                    nodes.push(value);
+                })
+                
             }
-
-            nodes.splice(nodeIndex, 1);
-            nodes = nodes.concat(maxWeightNode.children());
-            console.log(nodes.length);
+            return nodes;
         }
-        return nodes;
-    }
 
-    function weight(node) {
-        var height = $(node).height();
-        var width = $(node).width();
-        var s = height * width;
-        var lengthText = s / 140;
+        function getMaxWeightBlock(nodes) {
+            var maxWeight = weight(nodes[0]);
+            var maxWeightNode = nodes[0]; 
+            if (nodes.length > 1) {
+                for(var i = 1; i < nodes.length; i++) {
+                    var currentWeight = weight(nodes[i]);
+                    if (maxWeight < currentWeight) {
+                       maxWeightNode = nodes[i];
+                       break;
+                    };
+                }
+            }
+            return maxWeightNode;
+        }
 
-        return Math.log(lengthText * 0.1 + 1.01) * Math.log(s * 0.9 + 0.11);
-    }
+        function markSelectedBlocks(nodes) {
+            $.each(nodes, function( index, value ) {
+                $(value).addClass('semBlock');
+            });
+        }
+
+        function weight(node) {
+            var height = $(node).height();
+            var width = $(node).width();
+            var s = height * width;
+            var lengthText = s / 140;
+
+            return Math.log(lengthText * 0.1 + 1.01) * Math.log(s * 0.9 + 0.11);
+        }
